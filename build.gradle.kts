3bfda7103a0e7e6524e5867bb7aa3454547512fd
Lars Reimann <mail@larsreimann.com>
2022-01-08 17:43:59 +0100

    build: use in-memory GPG keys

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,7 @@ kover {
 
 subprojects {
     group = "com.larsreimann"
-    version = "0.1.0"
+    version = "1.0.0-SNAPSHOT"
 
     repositories {
         mavenCentral()
--- modeling-core/build.gradle.kts
+++ modeling-core/build.gradle.kts
@@ -76,6 +76,9 @@ publishing {
 }
 
 signing {
+    val signingKey: String? by project
+    val signingPassword: String? by project
+    useInMemoryPgpKeys(signingKey, signingPassword)
     sign(publishing.publications["mavenJava"])
 }
 
