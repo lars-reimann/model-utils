0ce80cbb231cc0a5e740c76344b07ad9758b4b8c
Lars Reimann <mail@larsreimann.com>
2022-09-14 14:06:31 +0200

    build: migrate kover

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,14 @@
 // Plugins -------------------------------------------------------------------------------------------------------------
 
 plugins {
-    id("org.jetbrains.kotlinx.kover") version "0.6.0"
-
     // Pin versions for subprojects
     kotlin("jvm") version "1.7.10" apply false
     id("org.jetbrains.dokka") version "1.7.10" apply false
+    id("org.jetbrains.kotlinx.kover") version "0.6.0" apply false
 }
 
 repositories {
     mavenCentral()
-}
-
-kover {
-    coverageEngine.set(kotlinx.kover.api.CoverageEngine.INTELLIJ)
 }
 
 // Subprojects ---------------------------------------------------------------------------------------------------------
